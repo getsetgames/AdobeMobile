4cd7713905cfc875105122eef9b5a15339bd23cc
Robert Segal <rob@getsetgames.com>
2016-04-15 11:58:23 -0400

    Defines two functions from SDK we are most likely to need for now.  Android is not setup yet will need to be tested at a later date.

--- Source/AdobeMobile/Private/AdobeMobileFunctions.cpp
+++ Source/AdobeMobile/Private/AdobeMobileFunctions.cpp
@@ -5,13 +5,49 @@
 
 #include "AdobeMobilePrivatePCH.h"
 
-#if PLATFORM_ANDROID
-
-// #include "Android/AndroidJNI.h"
-// #include "AndroidApplication.h"
-
-
-#elif PLATFORM_IOS
-
-
+#if PLATFORM_IOS
+#import "../../lib/iOS/ADBMobile.h"
+#elif PLATFORM_ANDROID
+#include "Android/AndroidJNI.h"
+#include "AndroidApplication.h"
 #endif
+
+void UAdobeMobileFunctions::AdobeMobileTrackState(FString state, FString data)
+{
+    UE_LOG(LogAdobeMobile, Log, TEXT("Tracking State..."));
+    
+#if PLATFORM_IOS
+    [ADBMobile trackState:@"" data:@{ }];
+#elif PLATFORM_ANDROID
+    if (JNIEnv* Env = FAndroidApplication::GetJavaEnv())
+    {
+        static jmethodID Method = FJavaWrapper::FindMethod(Env,
+                                                           FJavaWrapper::GameActivityClassID,
+                                                           "AndroidThunkJava_AdobeMobile_TrackState", "()Ljava/lang/String;",
+                                                           false);
+        
+        FJavaWrapper::CallObjectMethod(Env, FJavaWrapper::GameActivityThis, Method);
+    }
+#endif
+}
+
+void UAdobeMobileFunctions::AdobeMobileTrackAction(FString state, FString data)
+{
+    UE_LOG(LogAdobeMobile, Log, TEXT("Tracking Action..."));
+    
+#if PLATFORM_IOS
+    [ADBMobile trackAction:@"" data:@{ }];
+#elif PLATFORM_ANDROID
+    if (JNIEnv* Env = FAndroidApplication::GetJavaEnv())
+    {
+        static jmethodID Method = FJavaWrapper::FindMethod(Env,
+                                                           FJavaWrapper::GameActivityClassID,
+                                                           "AndroidThunkJava_AdobeMobile_TrackAction", "()Ljava/lang/String;",
+                                                           false);
+        
+        FJavaWrapper::CallObjectMethod(Env, FJavaWrapper::GameActivityThis, Method);
+    }
+#endif
+}
+
+
