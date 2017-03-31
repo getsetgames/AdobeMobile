a840fa1490a654b7268d009081ef6603efa83a84
Derek van Vliet <derek@getsetgames.com>
2017-03-31 14:30:24 -0400

    added required includes

--- Source/AdobeMobile/Classes/AdobeMobileComponent.h
+++ Source/AdobeMobile/Classes/AdobeMobileComponent.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include "Components/ActorComponent.h"
 #include "AdobeMobileFunctions.h"
 #include "AdobeMobileComponent.generated.h"
 
--- Source/AdobeMobile/Classes/AdobeMobileSettings.h
+++ Source/AdobeMobile/Classes/AdobeMobileSettings.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include "UObject/Object.h"
+#include "UObject/ScriptMacros.h"
 #include "AdobeMobileSettings.generated.h"
 
 UCLASS(config = Engine, defaultconfig)
